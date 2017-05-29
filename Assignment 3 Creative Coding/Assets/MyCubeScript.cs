b4c3a73b1cec0bc14c1a26a68b697c62ef7270ee
jchi517 <kuroba1121@gmail.com>
2017-05-30 10:49:28 +1200

    Module 3
    Finishing Module 3

--- Assignment 3 Creative Coding/Assets/MyCubeScript.cs
+++ Assignment 3 Creative Coding/Assets/MyCubeScript.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class MyCubeScript : MonoBehaviour {
 
+    public Vector3 spinSpeed = new Vector3(0, 0, 0);
+
 	// Use this for initialization
 	void Start () {
-		
+
+        this.transform.position = new Vector3(0, 5, 0);
+        this.transform.localScale = new Vector3(2.0f, 2.0f, 2.0f);
+        spinSpeed = new Vector3(Random.value, Random.value, Random.value);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        this.transform.Rotate(spinSpeed);
+
 	}
 }
